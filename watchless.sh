126780c8ea21706827e9fd0dbdaa23a0729b364f
mr-beaver <beavercoding@gmail.com>
2016-05-12 17:44:08 -0700

    gave a simple example in readme.md;

--- watchless.sh
+++ watchless.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 inotifywait -q -m -e close_write ./less/* |
 while read -r filename event; do
-  lessc ./less/stage-unideck.less ./main.css         # or "./$filename"
+  lessc ./less/stage-unideck.less ./main.css
 done
