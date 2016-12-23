2276d1e9730fa502b5610debe78511ccdadc065e
Tim Liu <bluekvirus@gmail.com>
2016-12-22 19:16:20 -0800

    Repo clean-up;

--- watchless.sh
+++ watchless.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+#
+# Note: You will need to install inotifywait (apt) and less (npm)
+#
+# @author Patrick Zhu, Tim Lauv
+# @created 2016.12.18
+
+COMPILE_CMD="lessc --verbose ./less/main.less ./stage-unideck.css"
+
+#1. Refresh main.css
+eval $COMPILE_CMD
+
+#2. Keep watching
 inotifywait -q -m -e close_write ./less/* |
 while read -r filename event; do
-  lessc --verbose ./less/stage-unideck.less ./main.css
+  eval $COMPILE_CMD
 done
